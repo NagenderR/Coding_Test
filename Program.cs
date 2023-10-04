6775de3bdaac536d09c7985815ee29261b191d93
kamal <kamal@LAPTOP-4R9ET7FE>
2023-10-04 18:36:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+/**Fizz Buzz Logic**/
+for (int i = 0; i < 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine(i.ToString() + "..fizzbuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(i.ToString() + "..fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(i.ToString() + "..buzz");
+    }
+}
+/**Fizz Buzz Logic**/
+Console.WriteLine("........................");
+/**String Reverse**/
+Console.Write("Enter string to reverse:");
+string input = Console.ReadLine();
+Console.WriteLine(input);
+StringBuilder reversedinput = new();
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    reversedinput.Append(input[i]);
+}
+Console.WriteLine(reversedinput.ToString());
+/**String Reverse**/
